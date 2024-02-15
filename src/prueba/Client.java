f3e8aaa0090b0040c6a4fe9ba574b56a5f72eef5
rvinocunga <adam22rvinocunga@inslaferreria.cat>
2024-02-15 19:10:32 +0100

    Responder varios clientes
    Hacer que el SERVIDOR CENTRAL responda a varios clientes, el codigo comentado es el anterior, NO BORRAR.

--- src/prueba/Client.java
+++ src/prueba/Client.java
@@ -1,11 +1,15 @@
 package prueba;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,6 +20,36 @@ import java.util.logging.Logger;
  */
 public class Client {
 
+    static Scanner teclat = new Scanner(System.in);
+
+    public static void main(String[] args) throws UnknownHostException, IOException {
+		// Creem una connexió amb el servidor
+		Socket connexio = new Socket("localhost", 7879);
+		System.out.println("Connexió amb el servidor establerta.");
+		
+		// Obrim els cananls de comunicació amb el servidor
+		DataInputStream in = new DataInputStream(connexio.getInputStream());
+		DataOutputStream out = new DataOutputStream(connexio.getOutputStream());
+		
+		// Enviar nombres al servidor
+		String msg = "";
+		do {
+			System.out.print("NOMBRE: ");
+			msg = teclat.nextLine();
+			if(msg=="") msg="FINAL";
+			out.writeUTF(msg);
+		} while(!msg.equals("FINAL"));
+		
+		// Rebre resposta del servidor
+		msg = in.readUTF();
+		System.out.println("SUMA TOTAL: "+msg);
+		
+		// Tanquem la connexió amb el client
+		in.close();
+		out.close();
+		connexio.close();
+		System.out.println("Connexió tancada");
+	}/*
     public static void main(String[] args) throws SocketException, IOException {
         Scanner teclat = new Scanner(System.in);
         boolean sortir = false;
@@ -44,16 +78,15 @@ public class Client {
                         System.out.println("\nInserta un número válido...\n");
                 }
             } else {
-                System.out.println("\nEscribe un numero...\n");
+                System.out.println("\nEteclatribe un numero...\n");
                 teclat.next();
             }
             System.out.println("");
         } while (!sortir);
 
     }
-
+*/
     private static void crearPartida() {
-        Scanner sc = new Scanner(System.in);
 
         //testeig
         InetAddress serverAddress;
@@ -62,15 +95,15 @@ public class Client {
         try {
             do {
                 System.out.println("\nIntroduce el puerto para jugar: ");
-                if (sc.hasNextInt()) {
+                if (teclat.hasNextInt()) {
                     break;
                 } else {
                     System.out.println("Introdueix un valor númeric...");
-                    sc.nextLine();
+                    teclat.nextLine();
                 }
             } while (true);
 
-            String msg = "CREAR " + sc.nextLine();
+            String msg = "CREAR " + teclat.nextLine();
 
             serverAddress = InetAddress.getByName("127.0.0.1");
 
@@ -92,6 +125,36 @@ public class Client {
     }
 
     private static void unirsePartida() {
+        try {
+            //testeig
+            InetAddress serverAddress;
+            int serverPort = 7879;
 
+            // mensaje que envia el cliente
+            String msg = "UNIR-ME ";
+
+            serverAddress = InetAddress.getByName("127.0.0.1");
+
+            //enviar missatge a servidor
+            DatagramSocket socket = new DatagramSocket();
+
+            byte[] bytesOUT = msg.getBytes();
+            DatagramPacket outPacket = new DatagramPacket(bytesOUT, bytesOUT.length,
+                    serverAddress, serverPort);
+            socket.send(outPacket);
+            
+            //
+
+            byte[] buffer = new byte[1024];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            // recibe paquete
+            socket.receive(packet);
+            System.out.println(new String(packet.getData()).trim());
+            
+        } catch (SocketException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- src/prueba/UDPServerCentral.java
+++ src/prueba/UDPServerCentral.java
@@ -1,24 +1,42 @@
 package prueba;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ *
+ * @author roger
+ */
 public class UDPServerCentral {
+    static int port = 7879;
 
-    public static void main(String[] args) throws SocketException {
-        /*
-        La seva funció serà posar en contacte a dos jugadors.
-        Per fer-ho escoltarà peticions UDP pel port 7879. 
-        Les peticions que rebrà tindran la forma:
-         */
-        
-        //El servidor escolta...
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(port);
+        while (true) {
+            // Esperem que es connecti un client
+            System.out.println("Esperant nou client..." + port);
+            Socket connexio = server.accept();
+            System.out.println("Client " + connexio.getInetAddress().getHostAddress() + " connectat.");
+            new ComunicacioClient(connexio).start();
+        }
+    }
+
+    /*
+    public static void main(String[] args) throws SocketException, InterruptedException {
+
         int port = 7879;
         DatagramSocket socket = new DatagramSocket(port);
-        System.out.printf("Escoltant al port %d...", port);
-        
+
+        List ipPartidesEnCola = new ArrayList();
+
         while (true) {
             byte[] buffer = new byte[1024];
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
@@ -29,7 +47,7 @@ public class UDPServerCentral {
                 String msg = new String(packet.getData()).trim();
                 String ipClient = packet.getAddress().getHostAddress();
                 System.out.printf("%s --> %s\n", ipClient, msg);
-                
+
                 // se corta el mensaje (CREAR 123)
                 String[] missatgeTallat = msg.split(" ");
 
@@ -40,10 +58,35 @@ public class UDPServerCentral {
                     case "CREAR":
                         System.out.println("\nEntra a la opción CREAR");
                         int portPerJugar = Integer.parseInt(missatgeTallat[1]);
-                        System.out.println("Port per jugar: " + ipClient + ":"+ portPerJugar);
+                        // emmagatzema en una List les IPs en cua per a jugar
+                        ipPartidesEnCola.add(ipClient + "::" + portPerJugar);
+                        System.out.println("Port per jugar: " + ipClient + ":" + portPerJugar);
                         break;
                     case "UNIR-ME":
-                        System.out.println("\nEntra a opción UNIR-ME");
+                        System.out.println("\nEntra a opción UNIR-ME...");
+                        byte[] bytesOUT;
+                        DatagramPacket outPacket;
+
+                        do {
+                            msg = "Buscando partida...";
+                            System.out.println(msg);
+
+                            bytesOUT = msg.getBytes();
+                            System.out.println("El cliente: " + packet.getSocketAddress().toString() + " está esperando...");
+
+                            outPacket = new DatagramPacket(bytesOUT, bytesOUT.length, packet.getSocketAddress());
+                            socket.send(outPacket);
+
+                            // mientras no haya nada en la List, se ejecutarà hasta que haya algo cada 5 segundos
+                            Thread.sleep(5 * 1000);
+                        } while (ipPartidesEnCola.isEmpty());
+
+                        msg = "Se ha encontrado una partida!";
+                        System.out.println(msg);
+                        bytesOUT = msg.getBytes();
+                        outPacket = new DatagramPacket(bytesOUT, bytesOUT.length, packet.getSocketAddress());
+                        socket.send(outPacket);
+
                         break;
                     default:
                         throw new AssertionError();
@@ -53,5 +96,51 @@ public class UDPServerCentral {
                 System.out.println("Error: " + e.getMessage());
             }
         }
+    }*/
+
+    private static class ComunicacioClient extends Thread {
+
+        private Socket connexio;
+
+        public ComunicacioClient(Socket connexio) {
+            this.connexio = connexio;
+        }
+
+        @Override
+        public void run() {
+            String addr = this.connexio.getInetAddress().getHostAddress() + ":" + String.valueOf(this.connexio.getPort());
+            try {
+                // Obrim els cananls de comunicació amb el client
+                DataInputStream in = new DataInputStream(connexio.getInputStream());
+                DataOutputStream out = new DataOutputStream(connexio.getOutputStream());
+
+                // Repetir l'intercanvi de dades mentre el client estigui connectat.
+                int suma = 0;
+
+                String msg = in.readUTF();
+                while (!msg.equals("FINAL")) {
+                    try {
+                        suma += Integer.parseInt(msg);
+                        System.out.println(addr + " --> Nombre rebut: " + msg);
+                    } catch (NumberFormatException e) {
+                        System.out.println(addr + " --> Nombre mal format: " + msg);
+                    }
+
+                    msg = in.readUTF();
+                }
+
+                System.out.println(addr + " --> Total suma: " + suma);
+                out.writeUTF(String.valueOf(suma));
+
+                // Tanquem la connexió amb el client
+                in.close();
+                out.close();
+                connexio.close();
+                System.out.println(addr + " --> Connexió tancada");
+            } catch (IOException e) {
+                System.out.println(addr + " --> Client desconnectat.");
+            }
+        }
+
     }
 }
